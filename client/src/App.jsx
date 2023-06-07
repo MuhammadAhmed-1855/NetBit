dc4c1a485ac8e015dc9d149fc90f877a087256d2
MuhammadAhmed-1855 <i201855@nu.edu.pk>
2023-06-07 05:57:32 +0500

    Second Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
-function App() {
+import "react-toastify/dist/ReactToastify.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const App = () => {
   return (
     <div className="App">
       <h1>NetBit | Watch Free Movies</h1>
