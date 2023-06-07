c5566663a18c5a3c7f6c5065fef3899eeca9b70f
MuhammadAhmed-1855 <i201855@nu.edu.pk>
2023-06-07 07:09:49 +0500

    Sixth Commit {Adding TMDB API and index.js Backend}

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import http from "http";
 import mongoose from "mongoose";
 import "dotenv/config";
+import routes from "./src/routes/index.js";
 
 const app = express();
 
@@ -12,16 +13,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use("/api/v1", routes);
+
 const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log("Connected to MongoDB");
-    server.listen(port, () => {
-        console.log(`Server is running on port: ${port}`);
-    });
-}).catch((error) => {
-    console.log(error.message);
-    process.exit(1);
+  console.log("Mongodb connected");
+  server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}).catch((err) => {
+  console.log({ err });
+  process.exit(1);
 });
