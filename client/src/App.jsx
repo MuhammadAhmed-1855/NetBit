4ce9ecf545e2fe097155c4cfaa32125101f3a790
MuhammadAhmed-1855 <i201855@nu.edu.pk>
2023-06-07 06:54:55 +0500

    Fifth Commit {Adding Topbar Functionality}

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,11 @@
+import { ThemeProvider } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import themeConfigs from "./configs/theme.configs";
+import { ToastContainer } from "react-toastify";
+import CssBaseline from "@mui/material/CssBaseline";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+
 import "react-toastify/dist/ReactToastify.css";
 import "swiper/css";
 import "swiper/css/navigation";
